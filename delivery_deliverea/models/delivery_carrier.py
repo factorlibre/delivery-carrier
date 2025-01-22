fd8a304987bbe3f9c3c6c64c8a38b3f28d25bbf1
Álvaro Rollan <alvarorollan@factorlibre.com>
2025-01-22 16:55:41 +0100

    [UPD] add service_id on create

--- delivery_deliverea/models/delivery_carrier.py
+++ delivery_deliverea/models/delivery_carrier.py
@@ -286,7 +286,7 @@ class DeliveryCarrier(models.Model):
                     {},
                 )
                 if not service_id:
-                    self._create_service(
+                    service_id = self._create_service(
                         carrier_code, service_code, service_parameter, active_service
                     )
                 self.manage_deliverea_params(service_id, service_parameter)
